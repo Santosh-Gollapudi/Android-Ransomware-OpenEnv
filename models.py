8030ce4f265fd8edbb324ca99d87b8227ed5cdfd
Santosh Gollapudi <gsantosh@users.noreply.huggingface.co>
2026-04-25 10:13:50 +0000

    Update models.py

--- models.py
+++ models.py
@@ -2,11 +2,13 @@ from dataclasses import dataclass, field
 from typing import List
 from openenv.core.env_server.types import Action, Observation, State
 
+
 @dataclass
 class AndroidAction(Action):
     action_type: str
     target_file_or_process: str
-    
+
+
 @dataclass
 class AndroidObservation(Observation):
     cpu_usage_percent: float
@@ -14,6 +16,7 @@ class AndroidObservation(Observation):
     suspicious_system_calls: List[str] = field(default_factory=list)
     is_device_compromised: bool = False
     system_log: str = ""
+
 
 @dataclass
 class AndroidState(State):
