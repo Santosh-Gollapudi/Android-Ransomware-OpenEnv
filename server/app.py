e752049c93d43f1a8139f076304300b22f14c04a
Santosh Gollapudi <gsantosh@users.noreply.huggingface.co>
2026-04-25 09:53:40 +0000

    Update server/app.py

--- server/app.py
+++ server/app.py
@@ -1,26 +1,11 @@
-try:
-    from openenv.core.env_server.http_server import create_app
-except Exception as e:
-    raise ImportError("openenv-core is required.") from e
+import os
+import sys
 
-from models import Action, Observation
-from server.ransomware_env_environment import RansomwareEnvironment
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-app = create_app(
-    RansomwareEnvironment,
-    Action,        
-    Observation,   
-    env_name="ransomware_env",
-    max_concurrent_envs=1, 
-)
+from models import AndroidAction, AndroidObservation
+from server.android_ransomware_env_environment import AndroidRansomwareEnvironment
+from openenv.core.env_server import create_web_interface_app
 
-def main():
-    import uvicorn
-    import argparse
-    parser = argparse.ArgumentParser()
-    parser.add_argument(--port, type=int, default=8000)
-    args = parser.parse_args()
-    uvicorn.run(app, host="0.0.0.0", port=args.port)
-
-if __name__ == '__main__':
-    main()
+env = AndroidRansomwareEnvironment()
+app = create_web_interface_app(env, AndroidAction, AndroidObservation)
