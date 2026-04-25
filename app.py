e5986ac0108f33dbec7a16b672ec30ad7bdf7254
Santosh Gollapudi <gsantosh@users.noreply.huggingface.co>
2026-04-25 10:11:31 +0000

    Create app.py

---
+++ app.py
@@ -0,0 +1,6 @@
+from server.android_ransomware_env_environment import AndroidRansomwareEnvironment
+from models import AndroidAction, AndroidObservation
+from openenv.core.env_server import create_web_interface_app
+
+env = AndroidRansomwareEnvironment()
+app = create_web_interface_app(env, AndroidAction, AndroidObservation)
