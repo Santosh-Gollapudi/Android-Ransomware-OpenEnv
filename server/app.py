120698f9347fca86a9c90be04604b52cdd7c8a0a
Santosh Gollapudi <gsantosh@users.noreply.huggingface.co>
2026-04-25 10:03:51 +0000

    Update server/app.py

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(os.path.abspath("."))
 
 from models import AndroidAction, AndroidObservation
 from server.android_ransomware_env_environment import AndroidRansomwareEnvironment
